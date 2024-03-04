4c8d64ebdd7d944054f9ce4c4ec72971f9ef609e
Aschay <ayadi.chayma@gmail.com>
2024-03-04 08:26:51 +0100

    implement oauth2 client with github as authorization server and using oauth2 login feature

---
+++ github-oauth2-client/src/main/java/com/aschay/secureoauthclient/SecureOauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.aschay.secureoauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureOauthClientApplication.class, args);
+	}
+
+}
---
+++ github-oauth2-client/src/test/java/com/aschay/secureoauthclient/SecureOauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aschay.secureoauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureOauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
