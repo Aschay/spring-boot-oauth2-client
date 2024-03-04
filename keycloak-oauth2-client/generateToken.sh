bceafa9337ac64a31b502815cac8dd4929c83fc5
Aschay <ayadi.chayma@gmail.com>
2024-03-04 13:02:34 +0100

    changes in keycloak port and others

--- keycloak-oauth2-client/generateToken.sh
+++ keycloak-oauth2-client/generateToken.sh
@@ -3,5 +3,5 @@ curl --location 'http://localhost:8081/realms/SpringBootKeycloak/protocol/openid
 --header 'Cookie: JSESSIONID=EE37EFB9E48FFC92C6FC37B76725F6F6' \
 --data-urlencode 'client_id=login-app' \
 --data-urlencode 'username=user1' \
---data-urlencode 'password=xsw2@WS' \
+--data-urlencode 'password=user@1' \
 --data-urlencode 'grant_type=password'
--- keycloak-oauth2-client/startKeyCloakContainer.sh
+++ keycloak-oauth2-client/startKeyCloakContainer.sh
@@ -1,4 +1,4 @@
-docker run -p 8082:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -d  --name keycloak quay.io/keycloak/keycloak:23.0.7 start-dev 
+docker run -p 8081:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -d  --name keycloak quay.io/keycloak/keycloak:23.0.7 start-dev 
 read -p "Press enter to continue"
 containerName=keycloak
 docker logs --follow $containerName
